d0d5b55ceab4f33e79f32c6f28ab61c4402d18f5
Mehdi Mohammadi <mhimhdi@yahoo.com>
2021-08-05 00:25:55 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,92 @@
-gsap.registerPlugin(MotionPathPlugin);
-MorphSVGPlugin.convertToPath("circle, rect");
-gsap.set("#paperPlaneRoute", { drawSVG: "0% 0%" });
-gsap.set("#rectSentItems", { x: "-=240" });
-const tl = gsap.timeline();
+// ICONS CALLBACK
+feather.replace();
 
-let ranOnce = false;
+// SVGs
+$('#svgDribbble').load('https://s3-us-west-2.amazonaws.com/s.cdpn.io/373860/references.html #dribbble');
+$('#svgTwitter').load('https://s3-us-west-2.amazonaws.com/s.cdpn.io/373860/references.html #twitter');
+$('#svgHicon').load('https://s3-us-west-2.amazonaws.com/s.cdpn.io/373860/references.html #hicon');
 
-function onBtnUp() {
-	if (ranOnce) {
-		tl.restart();
-		return;
-	}
-	ranOnce = true;
-	tl.to("#base", { duration: 0.2, scale: 1, transformOrigin: "50% 50%" });
-	tl.to(
-		"#btnBase",
-		{ duration: 0.77, morphSVG: "#cBottom", ease: "power1.inOut" },
-		"start"
-	);
+// NEW CODE HERE
 
-	tl.to("#btnBase", { duration: 0.23, morphSVG: "#cTop", ease: "power1.inOut" });
-	tl.to("#btnBase", {
-		duration: 0.2,
-		morphSVG: "#cCenter",
-		ease: "power1.inOut"
-	});
-	tl.to(
-		"#btnBase",
-		{ duration: 0.5, morphSVG: "#cEnd", ease: "power1.inOut" },
-		"revealStart"
-	);
-	tl.to("#rectSentItems", { x: "0", duration: 0.5 }, "revealStart");
-	tl.to(
-		"#mask1",
-		{ x: "-=260", duration: 0.5, ease: "power1.inOut" },
-		"revealStart"
-	);
-	tl.to(
-		"#paperPlane",
-		{ x: "-=205", duration: 0.5, ease: "power1.inOut" },
-		"revealStart"
-	);
-	tl.to(
-		"#paperPlanePath",
-		{ duration: 0.43, morphSVG: "#tickMark" },
-		"start+=0.77"
-	);
+// Variables
+var btnSz = 200;
+var c = 0;
+var bezierEasing = 'cubic-bezier(.69,-0.49,0,1)';
+var t1 = 299;
+var t2 = 1198;
 
-	tl.to(
-		"#txtSend",
-		{ duration: 0.6, scale: 0, transformOrigin: "50% 50%" },
-		"start"
-	);
-	tl.to(
-		"#paperPlaneRoute",
-		{ drawSVG: "80% 100%", duration: 0.7, ease: "power1.inOut" },
-		"start+=0.3"
-	);
-	tl.to(
-		"#paperPlaneRoute",
-		{ drawSVG: "100% 100%", duration: 0.2, ease: "power1.inOut" },
-		"start+=1"
-	);
+var bZ2 = 'cubic-bezier(.46,.56,0,.88)'
 
-	tl.to(
-		"#paperPlane",
-		{
-			duration: 1,
-			ease: "power1.inOut",
-			immediateRender: true,
-			motionPath: {
-				path: "#paperPlaneRoute",
-				align: "#paperPlaneRoute",
-				alignOrigin: [0.5, 0.5],
-				autoRotate: 90
-			}
-		},
-		"start"
-	);
+$('#mainWrap').on('click', function() {
+  c++;
+  console.log(c);
+  
+  // Lock
+  $('.lock').addClass('lock-on');
+  setTimeout(function() {
+    $('.lock').removeClass('lock-on');
+  }, 1200);
+  
+  // Wave
+  $('.wave').addClass('wave-act');
+  setTimeout(function() {
+    $('.wave').removeClass('wave-act');
+  }, 1000);
 
-	tl.to(
-		"#paperPlanePath",
-		{ duration: 0.15, attr: { fill: "#ffffff" } },
-		"start"
-	);
-	tl.to(
-		"#paperPlanePath",
-		{ duration: 0.15, attr: { fill: "#4E67E8" } },
-		"start+=0.77"
-	);
-}
+  // Move Button
+  setTimeout(function() {
+    $('#btn').transition({
+      x: '+=10px'
+    }, 289, bZ2, function() {
+      $('#btn').transition({
+        x: '0px'
+      }, 289, bZ2);
+    }); 
+  }, 399);
 
-function onBtnDown() {
-	gsap.timeline({ defaults: { clearProps: true } });
-	gsap.to("#base", { duration: 0.1, scale: 0.9, transformOrigin: "50% 50%" });
-}
 
-const btn = document.getElementById("base");
-btn.addEventListener("mousedown", onBtnDown);
-btn.addEventListener("mouseup", onBtnUp);
+  // Move Content
+  setTimeout(function() {
+    if (c <= 2) {
+      $('#content').transition({
+        x: '+=220px'
+      }, t1, bezierEasing, function() {
+        $('#content').transition({
+          x: '-=20'
+        }, 699, 'cubic-bezier(.25,.49,.2,1)');
+      });
+
+    } else if (c == 3) {
+      $('#content').transition({
+        x: '+=220px'
+      }, t1, bezierEasing, function() {
+        $('#content').transition({
+          x: '-=20'
+        }, 699, 'cubic-bezier(.25,.49,.2,1)');
+      });
+      setTimeout(function() {
+        $('.cnt-1').css('order', '4');
+        $('#content').transition({
+          x: '0'
+        }, 0);
+      }, t2);
+    } else if (c == 4) {
+      $('#content').transition({
+        x: '+=220px'
+      }, t1, bezierEasing, function() {
+        $('#content').transition({
+          x: '-=20'
+        }, 699, 'cubic-bezier(.25,.49,.2,1)');
+      });
+      setTimeout(function() {
+        $('#content').transition({
+          x: '0'
+        }, 0);
+        $('.cnt-1').css('order', '0');
+      }, t2);
+      c = 0;
+    } 
+  }, 399)
+
+
+});
